bc751475671efc377cdc3c72454cdef34ec85d89
Sourav kumar <161188554+sourav842741@users.noreply.github.com>
2025-09-15 12:26:03 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,7 +27,7 @@ import updateLocation from './hooks/updateLocation'
 import TrackOrderPage from './pages/TrackOrderPage'
 import MyDeliveredOrders from './pages/MyDeliveredOrders'
 import ShopItems from './pages/ShopItems'
-export const serverUrl="http://localhost:8000"
+export const serverUrl="https://quick-zaikaaa.onrender.com"
 
 function App() {
   const {userData,allShops,socket}=useSelector(state=>state.user)
